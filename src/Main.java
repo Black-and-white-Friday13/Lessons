99d135835604046ee723c3bf2e5cb252e8535cf0
Black-and-white-Friday13 <vFriday13v@gmail.com>
2016-09-28 00:05:23 +0300

    new changes

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * Created by abegansky on 12.09.16.
- */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
